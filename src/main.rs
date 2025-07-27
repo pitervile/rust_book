08b83efbf2bcc86fd0aec52c768cc42acaf3986d
pitervile <piter.vile@gmail.com>
2025-07-27 21:00:29 +0200

    Refactor main function to improve OS detection logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
+    if cfg!(target_os = "macos") {
+        println!("You are running MacOS!");
+    } else if cfg!(target_os = "windows") {
+        println!("You are running Windows!");
+    } else {
+        println!("This is not MacOS or Windows.");
+    }
+
     println!("Hello, world!");
+}
+
+#[cfg(target_os = "macos")]
+fn macos_only() {
+  println!("You are running MacOS!");
+}
+
+#[cfg(target_os = "windows")]
+fn windows_only() {
+  println!("You are running Windows!");
 }
