7e3438fd7bf041acf580f3fbfa42a988e4fb01be
pitervile <piter.vile@gmail.com>
2025-08-07 15:51:23 +0200

    Czyszczenie gry

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,8 @@ fn main() {
         println!("This is not MacOS or Windows.");
     }
 
-    println!("Budzisz się w Lunolesie na rozdrożu wydeptanych ścieżek. Możesz iść na północ, wschód lub południe. Wybierz kierunek (n/e/s):");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let direction = input.trim().to_lowercase();
-    match direction.as_str() {
-        "n" => println!("Spotykasz Piotra wilkołaka-przytulaka. On jest bardzo przyjazny! Jeśli go pogłaszczesz, może cię poprowadzić dalej. Liże cię po twarzy i daje ci siłę!"),
-        "e" => println!("Spotykasz Piotra Wschodzące Słońce. Daje Ci siły i motywację do dalszej wędrówki. Całuje Cię swoimi promieniami i obiecuje, że zawsze będzie świecić na twojej drodze."),
-        "s" => println!("Spotykasz Piotra Południowego Wiatr. On jest trochę tajemniczy, ale może podzielić się z tobą swoją mądrością. Otula cię swoimi skrzydłami i szepcze, że zawsze będziesz miał wsparcie w trudnych chwilach."),
-        _ => println!("Nie rozumiem tego kierunku!"),
-    }
+    println!("");
+    
 }
 
 #[cfg(target_os = "macos")]
